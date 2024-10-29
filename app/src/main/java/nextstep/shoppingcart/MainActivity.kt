1e2c16de15858e0151c71f6f298e94fdbb367167
JoYehyun <jo990714@naver.com>
2024-10-29 17:56:32 +0900

    feat: 상품 아이템 목록 구현

--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -3,6 +3,13 @@ package nextstep.shoppingcart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -12,8 +19,11 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import nextstep.shoppingcart.ui.component.ProductItem
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +51,9 @@ fun ShoppingProductsScreen() {
                 },
                 actions = {
                     IconButton(
-                        onClick = {},
+                        onClick = {
+                            // TODO: 장바구니 이동
+                        },
                     ) {
                         Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = null)
                     }
@@ -49,7 +61,17 @@ fun ShoppingProductsScreen() {
             )
         },
     ) { paddingValues ->
-        paddingValues
+        LazyVerticalGrid(
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            items(products) { product ->
+                ProductItem(modifier = Modifier.padding(6.dp), product = product)
+            }
+        }
     }
 }
 
